fb1a46b3af0c3489190666df64105e5528bdd535
Jiuyang liu <liujiuyang1994@gmail.com>
2020-01-13 16:02:33 +0000

    add bscane2 for jtag chain

--- playground/src/FPGA.scala
+++ playground/src/FPGA.scala
@@ -1,9 +1,7 @@
 package arty100t
 
-import chisel3._
 import freechips.rocketchip._
 import amba.axi4.AXI4Bundle
-import chisel3.experimental._
 import diplomacy._
 import devices.debug.SystemJTAGIO
 import sifive.blocks.devices._
@@ -11,6 +9,89 @@ import uart._
 import spi._
 import playground._
 import sifive.fpgashells.ip.xilinx._
+import chisel3._
+import chisel3.util._
+import chisel3.experimental._
+
+class BUFGCE extends ExtModule {
+  val O = IO(Output(Bool()))
+  val CE = IO(Input(Bool()))
+  val I = IO(Input(Bool()))
+}
+
+class BSCANE2 extends ExtModule(Map("JTAG_CHAIN" -> 4)) {
+  val TDO = IO(Input(Bool()))
+  val CAPTURE = IO(Output(Bool()))
+  val DRCK = IO(Output(Bool()))
+  val RESET = IO(Output(Bool()))
+  val RUNTEST = IO(Output(Bool()))
+  val SEL = IO(Output(Bool()))
+  val SHIFT = IO(Output(Bool()))
+  val TCK = IO(Output(Bool()))
+  val TDI = IO(Output(Bool()))
+  val TMS = IO(Output(Bool()))
+  val UPDATE = IO(Output(Bool()))
+}
+
+class BscanJTAG extends MultiIOModule {
+  val tck: Bool = IO(Output(Bool()))
+  val tms: Bool = IO(Output(Bool()))
+  val tdi: Bool = IO(Output(Bool()))
+  val tdo: Bool = IO(Input(Bool()))
+  val tdoEnable: Bool = IO(Input(Bool()))
+
+  val bscane2: BSCANE2 = Module(new BSCANE2)
+  val bscane2Capture = bscane2.CAPTURE
+  val bscane2Drck = bscane2.DRCK
+  val bscane2Reset = bscane2.RESET
+  val bscane2Runtest = bscane2.RUNTEST
+  val bscane2Sel = bscane2.SEL
+  val bscane2Tck = bscane2.TCK
+  tdi := bscane2.TDI
+  bscane2.TDO := Mux(tdoEnable, tdo, true.B)
+  val bscane2Tdo = bscane2.TDO
+
+  val bufgce = new BUFGCE
+  bufgce.I := bscane2.TCK
+  bufgce.CE := bscane2.SEL
+  tck := bufgce.O
+
+  val posClock: Clock = bscane2.TCK.asClock
+  val negClock: Clock = (!bscane2.TCK).asClock
+  /**
+   * This two wire will cross two clock domain,
+   * generated at [[posClock]], used in [[negClock]]
+   * */
+  val tdiRegisterWire = Wire(Bool())
+  val shiftCounterWire = Wire(0.U(7.W))
+  withReset(!bscane2.SHIFT) {
+    withClock(posClock) {
+      val shiftCounter = RegInit(0.U(7.W))
+      val posCounter = RegInit(0.U(8.W))
+      val tdiRegister = RegInit(false.B)
+      posCounter := posCounter + 1.U
+      when(posCounter >= 1.U && posCounter <= 7.U) {
+        shiftCounter := Cat(bscane2.TDI, shiftCounter >> 1 << 1)
+      }
+      when(posCounter === 0.U) {
+        tdiRegister := bscane2.TDI
+      }
+      tdiRegisterWire := tdiRegister
+      shiftCounterWire := shiftCounter
+    }
+    withClock(negClock) {
+      val negCounter = RegInit(0.U(8.W))
+      negCounter := negCounter + 1.U
+      tms := MuxLookup(negCounter, false.B, Array(
+        4.U -> tdiRegisterWire,
+        5.U -> true.B,
+        shiftCounterWire + 7.U -> true.B,
+        shiftCounterWire + 8.U -> true.B)
+      )
+    }
+  }
+
+}
 
 
 class FPGATop extends MultiIOModule {
