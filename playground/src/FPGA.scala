5c9a250f63c23fd8db1e34992defd6a132a34908
Jiuyang liu <liujiuyang1994@gmail.com>
2020-02-17 17:12:05 +0000

    remove upstreamed code

--- playground/src/FPGA.scala
+++ playground/src/FPGA.scala
@@ -10,55 +10,9 @@ import spi._
 import playground._
 import chisel3._
 import chisel3.util._
-import chisel3.experimental.{ExtModule, Analog, attach}
+import chisel3.experimental.{Analog, ExtModule}
+import sifive.fpgashells.shell._
 
-class AnalogToUInt(w: Int = 1) extends BlackBox with HasBlackBoxInline {
-  val io = IO(new Bundle {
-    val a = Analog(w.W)
-    val b = Output(UInt(w.W))
-  })
-  setInline(s"atu_${w.toString}.v",
-    s"""module AnalogToUInt (a, b);
-       |   inout [${w - 1}:0] a;
-       |   output [${w - 1}:0] b;
-       |   assign b = a;
-       |endmodule
-       |""".stripMargin)
-}
-
-object AnalogToUInt {
-  def apply(a: Analog): UInt = {
-    val a2b = Module(new AnalogToUInt(w = a.getWidth))
-    attach(a, a2b.io.a)
-    a2b.io.b
-  }
-}
-
-class UIntToAnalog(w: Int = 1) extends BlackBox with HasBlackBoxInline {
-  val io = IO(new Bundle {
-    val a = Analog(w.W)
-    val b = Input(UInt(w.W))
-    val b_en = Input(Bool())
-  })
-  require(w >= 1)
-  setInline(s"uta_${w.toString}.v",
-    s"""module UIntToAnalog (a, b, b_en);
-       |   inout [${w - 1}:0] a;
-       |   input [${w - 1}:0] b;
-       |   input b_en;
-       |   assign a = b_en ? b : $w'b${"z".repeat(w)};
-       |endmodule
-       |""".stripMargin)
-}
-
-object UIntToAnalog {
-  def apply(b: UInt, a: Analog, b_en: Bool): Unit = {
-    val a2b = Module(new UIntToAnalog(w = a.getWidth))
-    attach(a, a2b.io.a)
-    a2b.io.b := b
-    a2b.io.b_en := b_en
-  }
-}
 
 class BUFGCE extends ExtModule {
   val O = IO(Output(Bool()))
@@ -165,7 +119,6 @@ class FPGATop extends MultiIOModule {
       UIntToAnalog(topSPI.dq(i).o, io, topSPI.dq(i).oe)
       topSPI.dq(i).i := AnalogToUInt(io)
   }
-
 
   val topJtag: SystemJTAGIO = top.debug.head.systemjtag.head
   val fpgaJtag = Module(new BscanJTAG)
