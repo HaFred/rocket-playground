ce6d3f9d6d0adf31d8d97f7185fe2074c9310511
Jiuyang liu <liujiuyang1994@gmail.com>
2020-04-16 12:45:21 +0000

    debuging diplomatictester dependency api

--- playground/src/unittester/play.scala
+++ playground/src/unittester/play.scala
@@ -1,15 +1,34 @@
 package unittester
 
 import freechips.rocketchip._
+import config._
 import diplomacy._
 import playground._
 import chisel3._
-import fpga.{CustomArty100TConfig, CustomArty100TRocketSystem}
+import chisel3.experimental.DataMirror
+import chiseltest._
+import chiseltest.RawTester
+import chiseltest.internal.{VerilatorBackendAnnotation, WriteVcdAnnotation}
+import diplomatictester._
+import diplomatictester.Utils._
+import fpga.{CustomArty100TConfig, CustomArty100TRocketSystem, CustomArty100TRocketSystemModuleImp}
+import freechips.rocketchip.subsystem.{BaseSubsystem, WithInclusiveCache}
+import logger.{LogLevel, LogLevelAnnotation}
 import os._
+import sifive.blocks.inclusivecache.InclusiveCache
 
+class CustomArty100TRocketSystemDut(implicit p: Parameters) extends CustomArty100TRocketSystem { lm =>
+  override lazy val module = new CustomArty100TRocketSystemModuleImp(this) {
+    val l2 = lm.childrenFinder(_.name == "l2").asInstanceOf[InclusiveCache]
+    dutModule(l2.module)
+    val dutAuto = dutIO(l2.module.auto, "dutAuto")
+  }
+}
 
-object InclusiveDut extends App {
-  val lm = LazyModule(configToRocketModule(classOf[CustomArty100TRocketSystem], new CustomArty100TConfig))
-  (new chisel3.stage.ChiselStage).emitChirrtl(lm.module)
-  write(pwd / "circuit" / "graph", lm.graphML)
+object InclusiveCacheTester extends App {
+  val lm = LazyModule(configToRocketModule(classOf[CustomArty100TRocketSystemDut], new CustomArty100TConfig))
+  RawTester.test(lm.module, Seq(WriteVcdAnnotation, LogLevelAnnotation(LogLevel.Info))) {
+    c =>
+      c.clock.step()
+  }
 }
